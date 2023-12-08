caa7f98923f4dafbc0c552c5fcf4f70dd630a18c
Anton Korobeynikov <anton@korobeynikov.info>
2023-12-08 12:51:48 -0800

    Rename to f2sz and get rid of TAR functionality

--- src/f2sz.c
+++ src/f2sz.c
@@ -1,11 +1,8 @@
 /* ******************************************************************
- * t2sz
+ * f2sz
  * Copyright (c) 2020, Martinelli Marco
- *
- * You can contact the author at :
- * - Email: marco+t2sz@13byte.com
- * - Source repository : https://github.com/martinellimarco/t2sz
- *
+ * Copyright (c) 2023, Anton Korobeynikov
+ * 
  * This source code is licensed under the GPLv3 (found in the LICENSE
  * file in the root directory of this source tree).
 ****************************************************************** */
@@ -22,57 +19,6 @@
 #include <sys/mman.h>
 #include <zstd.h>
 
-typedef struct __attribute__((__packed__)) { /* byte offset */
-    char name[100];               /*   0 */
-    char mode[8];                 /* 100 */
-    char uid[8];                  /* 108 */
-    char gid[8];                  /* 116 */
-    char size[12];                /* 124 */
-    char mtime[12];               /* 136 */
-    char chksum[8];               /* 148 */
-    char typeflag;                /* 156 */
-    char linkname[100];           /* 157 */
-    char magic[6];                /* 257 */
-    char version[2];              /* 263 */
-    char uname[32];               /* 265 */
-    char gname[32];               /* 297 */
-    char devmajor[8];             /* 329 */
-    char devminor[8];             /* 337 */
-    char prefix[155];             /* 345 */
-    /* 500 */
-} TarHeader;
-
-uint32_t checksum(TarHeader* header){
-    uint8_t* ptr = (uint8_t*)header;
-    uint32_t ac = 0;
-
-    while (ptr < (uint8_t*)&header->chksum) ac += *ptr++;
-
-    ac += 8*0x20;//8 ASCII spaces
-    ptr+= 8;
-
-    while (ptr < (uint8_t*)header+512) ac += *ptr++;
-
-    return ac;
-}
-
-bool isTarHeader(TarHeader* header){
-    uint32_t chksum = checksum(header);
-
-    char* buf = malloc(7);
-    memcpy(buf, header->chksum, 6);
-    buf[6] = 0;
-    uint32_t hdrChksum = strtoul(buf, NULL, 8);
-    free(buf);
-
-    if(chksum != hdrChksum){
-        fprintf(stderr, "ERROR: Mismatching checksum. Expected 0x%08x but found 0x%08x.\n", chksum, hdrChksum);
-        return false;
-    }
-
-    return true;
-}
-
 typedef struct SeekTableEntry SeekTableEntry;
 
 struct SeekTableEntry{
@@ -87,9 +33,7 @@ typedef struct {
     char *outFilename;
     uint8_t level;
     size_t minBlockSize;
-    size_t maxBlockSize;
     bool verbose;
-    bool rawMode; //non-tar mode
     uint32_t workers;
 
     //input buffer
@@ -284,62 +228,16 @@ void compressFile(Context *ctx){
     size_t residual = 0;
     while(!lastChunk) {
         size_t blockSize = 0;
-        if(ctx->rawMode){
-            if(ctx->minBlockSize){
-                blockSize = ctx->minBlockSize;
-                if(readBuff+blockSize > ctx->inBuff+ctx->inBuffSize){
-                    blockSize = ctx->inBuff+ctx->inBuffSize - readBuff;
-                    lastChunk = true;
-                }
-            }else{
-                blockSize = ctx->inBuffSize;
+
+        if(ctx->minBlockSize){
+            blockSize = ctx->minBlockSize;
+            if(readBuff+blockSize > ctx->inBuff+ctx->inBuffSize){
+                blockSize = ctx->inBuff+ctx->inBuffSize - readBuff;
                 lastChunk = true;
             }
         }else{
-            do{
-                if(residual){
-                    if(residual > ctx->maxBlockSize){
-                        blockSize = ctx->maxBlockSize;
-                        residual = residual - ctx->maxBlockSize;
-                    }else{
-                        blockSize = residual;
-                        residual = 0;
-                    }
-                }else if(ctx->inBuff[tarHeaderIdx]){//tar ends with null headers that we can skip
-                    TarHeader *header = (TarHeader *)&ctx->inBuff[tarHeaderIdx];
-                    if(isTarHeader(header)){
-                        size_t size = strtoul(header->size, NULL, 8);
-
-                        size_t mod = size%512;
-                        if(mod){
-                            size = size - mod + 512;
-                        }
-                        size_t toNextHeader  = size + 512;
-
-                        tarHeaderIdx += toNextHeader;
-                        blockSize += toNextHeader;
-                        
-                        if(ctx->maxBlockSize && blockSize > ctx->maxBlockSize){
-                            residual = blockSize - ctx->maxBlockSize;
-                            blockSize = ctx->maxBlockSize;
-                        }
-
-                        if(ctx->verbose){
-                            fprintf(stderr, "+ %s (%ld)\n", header->name, size);
-                        }
-                    }else{
-                        fprintf(stderr, "ERROR: Invalid tar header. If this is not a tar archive use raw mode (-r)\n");
-                        exit(-1);
-                    }
-                }else{
-                    if(ctx->verbose){
-                        fprintf(stderr, "+ <null>\n");
-                    }
-                    tarHeaderIdx+=512;
-                    blockSize += 512;
-                }
-                lastChunk = tarHeaderIdx >= ctx->inBuffSize;
-            }while(blockSize < ctx->minBlockSize && !lastChunk);
+            blockSize = ctx->inBuffSize;
+            lastChunk = true;
         }
 
         ZSTD_CCtx_setPledgedSrcSize(ctx->cctx, blockSize);
@@ -393,9 +291,9 @@ static char* getOutFilename(const char* inFilename){
 
 void version(){
     fprintf(stderr,
-            "t2sz version %s\n"
+            "f2sz version %s\n"
             "Copyright (C) 2020 Marco Martinelli <marco+t2sz@13byte.com>\n"
-            "https://github.com/martinellimarco/t2sz\n"
+            "Copyright (C) 2023 Anton Korobeynikov <anton+t2sz@korobeynikov.info>\n"
             "This software is distributed under the GPLv3 License\n"
             "THIS SOFTWARE IS PROVIDED \"AS IS\" WITHOUT ANY WARRANTY\n",
             VERSION);
@@ -407,38 +305,24 @@ void usage(const char *name, const char *str){
     }
 
     fprintf(stderr,
-            "t2sz: tar 2 seekable zstd.\n"
+            "f2sz: FASTA 2 seekable zstd.\n"
             "It compress a file into a seekable zstd, splitting the file into multiple frames.\n"
-            "If the file is a tar archive it compress each file in the archive into an independent frame, hence the name.\n"
-            "It operates in two modes. Tar archive mode and raw mode.\n"
-            "By default it runs in tar archive mode for files ending with .tar, unless -r is specified.\n"
-            "For all other files it runs in raw mode.\n"
-            "In tar archive mode it compress the archive keeping each file in a different frame, unless -s or -S is used.\n"
-            "This allows fast seeking and extraction of a single file without decompressing the whole archive.\n"
             "The compressed archive can be decompressed with any Zstandard tool, including zstd.\n"
             "\nTo take advantage of seeking see the following projects:\n"
             "\tC/C++ library:  https://github.com/martinellimarco/libzstd-seek\n"
             "\tPython library: https://github.com/martinellimarco/indexed_zstd\n"
-            "\tFUSE mount:     https://github.com/mxmlnkn/ratarmount\n"
             "\n"
             "Usage: %1$s [OPTIONS...] [TAR ARCHIVE]\n"
             "\n"
             "Examples:\n"
             "\t%1$s any.file -s 10M                        Compress any.file to any.file.zst, each input block will be of 10M\n"
-            "\t%1$s archive.tar                            Compress archive.tar to archive.tar.zst\n"
-            "\t%1$s archive.tar -o output.tar.zst          Compress archive.tar to output.tar.zst\n"
-            "\t%1$s archive.tar -o /dev/stdout             Compress archive.tar to standard output\n"
+            "\t%1$s any.file -o output.file.zst            Compress any.file to any.file.zst\n"
+            "\t%1$s any.file -o /dev/stdout                Compress any.file to standard output\n"
             "\n"
             "Options:\n"
             "\t-l [1..22]         Set compression level, from 1 (lower) to 22 (highest). Default is 3.\n"
             "\t-o FILENAME        Output file name.\n"
             "\t-s SIZE            In raw mode: the exact size of each input block, except the last one.\n"
-            "\t                   In tar mode: the minimum size of an input block, in bytes.\n"
-            "\t                                A block is composed by one or more whole files.\n"
-            "\t                                A file is never truncated unless -S is used.\n"
-            "\t                                If not specified one block will contain exactly one file, no matter the file size.\n"
-            "\t                                Each block is compressed to a zstd frame but if the archive has a lot of small files\n"
-            "\t                                having a file per block doesn't compress very well. With this you can set a trade off.\n"
             "\t                   The greater is SIZE the smaller will be the archive at the expense of the seek speed.\n"
             "\t                   SIZE may be followed by the following multiplicative suffixes:\n"
             "\t                       k/K/KiB = 1024\n"
@@ -447,17 +331,9 @@ void usage(const char *name, const char *str){
             "\t                       kB/KB = 1000\n"
             "\t                       MB = 1000^2\n"
             "\t                       GB = 1000^3\n"
-            "\t-S SIZE            In raw mode: it is ignored.\n"
-            "\t                   In tar mode: the maximum size of an input block, in bytes.\n"
-            "\t                   Unlike -s this option may split big files in smaller chuncks.\n"
-            "\t                   Remember that each block is compressed independently and a small value here will result in a bigger archive.\n"
-            "\t                   -S can be used together with -s but MUST be greater or equal to it's value.\n"
-            "\t                   If -S and -s are equal the input block will be of exactly that size, if there is enough input data.\n"
-            "\t                   Like -s SIZE may be followed by one of the multiplicative suffixes described above.\n"
             "\t-T [1..N]          Number of thread to spawn. It improves compression speed but cost more memory. Default is single thread.\n"
-            "\t                   It requires libzstd >= 1.5.0 or an older version compiler with ZSTD_MULTITHREAD.\n"
+            "\t                   It requires libzstd >= 1.5.0 or an older version compiled with ZSTD_MULTITHREAD.\n"
             "\t                   If `-s` or `-S` are too small it is possible that a lower number of threads will be used.\n"
-            "\t-r                 Raw mode or non-tar mode. Treat tar archives as regular files, without any special handling.\n"
             "\t-j                 Do not generate a seek table.\n"
             "\t-v                 Verbose. List the elements in the tar archive and their size.\n"
             "\t-f                 Overwrite output without prompting.\n"
@@ -519,22 +395,11 @@ int main(int argc, char **argv){
                 }
                 break;
             }
-            case 'S': {
-                size_t multiplier = decodeMultiplier(optarg);
-                ctx->maxBlockSize = atoi(optarg) * multiplier;
-                if(ctx->maxBlockSize < multiplier){
-                    usage(executable, "ERROR: Invalid block size");
-                }
-                break;
-            }
             case 'T':
                 ctx->workers = atoi(optarg);
                 if(ctx->workers<1){
                     usage(executable, "ERROR: Invalid number of threads. Must be greater than 0.");
                 }
-                break;
-            case 'r':
-                ctx->rawMode = true;
                 break;
             case 'j':
                 ctx->skipSeekTable = true;
@@ -563,15 +428,7 @@ int main(int argc, char **argv){
         usage(executable, "Too many arguments");
     }
     
-    if(ctx->maxBlockSize && ctx->maxBlockSize < ctx->minBlockSize){
-        usage(executable, "The maximum block size can't be smaller than the minimum one");
-    }
-
     ctx->inFilename = argv[0];
-
-    if(!ctx->rawMode){
-        ctx->rawMode = !strEndsWith(ctx->inFilename, "tar");
-    }
 
     if(access(ctx->inFilename, F_OK ) != 0){
         fprintf(stderr, "%s: File not found\n", ctx->inFilename);
